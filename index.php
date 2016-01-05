ad83018e3d46823630b828d99558d1b48bd3ea8a
Oguzhan Tasci <oguzhantasc@gmail.com>
2016-01-05 18:46:27 +0200

    added index and styles

---
+++ index.php
@@ -0,0 +1,49 @@
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="master.css">
+	<meta charset="utf-8">
+</head>
+<body>
+	<div class="nav">
+		<div>
+			<a href="index.html">ingilizce kelimelik</a>
+		<a class="right" href="kayit.php">Kayıt Ol</a>
+		<a class="right2" href="giris.php">Giriş Yap</a>
+		</div>
+	</div>
+	<div class="main">
+		<div class="sol">
+		<h3 class="center">Panel</h3>
+		<ul>
+			<li><a class="a-white" href="">Random Box</a></li>
+			<li><a class="a-white" href="">Create Box</a></li>
+			<li><a class="a-white" href="">Show Your Boxes</a></li>
+
+		</ul>
+	</div>
+	<ul class="icerik">
+        <div id="resim-don">
+            <li class="resim">
+                <p>Hello!</p>
+            </li>
+            <li class="aciklama">
+                <p>Anlamı: Merhaba!</p>
+                <p>Örnek Cümle: Hello! Dear friends! </p>
+            </li>
+        </div>
+    </ul>
+	<div class="gobek2">
+		<a href="">Random Vocabulary</a>
+		<br>
+		<a href="">English - Turkish Mode</a>
+		<br>
+		<a href="">Turkish - English Mode</a>
+	</div>
+	</div>
+	<div class="oneri">
+		<h3 class="center">Recommended Boxes</h3>
+	</div>
+	<div class="oneri-box center"><a class="a-white" href="">Box 1</a></div>
+</body>
+</html>
