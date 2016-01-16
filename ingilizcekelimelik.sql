e4da0215563250e738f59ff6ace0106fff1a6e86
Oguzhan Tasci <oguzhantasc@gmail.com>
2016-01-16 18:42:16 +0200

    fixed user table name

--- ingilizcekelimelik.sql
+++ ingilizcekelimelik.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost:8889
--- Generation Time: Jan 16, 2016 at 05:25 PM
+-- Generation Time: Jan 16, 2016 at 05:41 PM
 -- Server version: 5.5.42
 -- PHP Version: 5.6.10
 
@@ -31,20 +31,34 @@ CREATE TABLE `box` (
   `u_id` int(11) NOT NULL,
   `v_id` int(11) NOT NULL,
   `b_name` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `box`
+--
+
+INSERT INTO `box` (`b_id`, `u_id`, `v_id`, `b_name`) VALUES
+(1, 1, 1, 'my box');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Table structure for table `user`
 --
 
-CREATE TABLE `users` (
+CREATE TABLE `user` (
   `u_id` int(11) NOT NULL,
   `username` varchar(100) NOT NULL,
   `password` text NOT NULL,
   `email` varchar(200) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`u_id`, `username`, `password`, `email`) VALUES
+(1, 'oguzhan', '123', 'oguzhantasc@gmail.com');
 
 -- --------------------------------------------------------
 
@@ -57,7 +71,14 @@ CREATE TABLE `vocabulary` (
   `v_name` text NOT NULL,
   `v_trans` text NOT NULL,
   `v_example` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `vocabulary`
+--
+
+INSERT INTO `vocabulary` (`v_id`, `v_name`, `v_trans`, `v_example`) VALUES
+(1, 'hello', 'merhaba', 'hello world');
 
 --
 -- Indexes for dumped tables
@@ -72,9 +93,9 @@ ALTER TABLE `box`
   ADD KEY `v_id` (`v_id`);
 
 --
--- Indexes for table `users`
+-- Indexes for table `user`
 --
-ALTER TABLE `users`
+ALTER TABLE `user`
   ADD PRIMARY KEY (`u_id`),
   ADD UNIQUE KEY `username` (`username`),
   ADD UNIQUE KEY `email` (`email`);
@@ -93,17 +114,17 @@ ALTER TABLE `vocabulary`
 -- AUTO_INCREMENT for table `box`
 --
 ALTER TABLE `box`
-  MODIFY `b_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `b_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 --
--- AUTO_INCREMENT for table `users`
+-- AUTO_INCREMENT for table `user`
 --
-ALTER TABLE `users`
-  MODIFY `u_id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `user`
+  MODIFY `u_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT for table `vocabulary`
 --
 ALTER TABLE `vocabulary`
-  MODIFY `v_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `v_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 --
 -- Constraints for dumped tables
 --
@@ -112,7 +133,7 @@ ALTER TABLE `vocabulary`
 -- Constraints for table `box`
 --
 ALTER TABLE `box`
-  ADD CONSTRAINT `box_ibfk_2` FOREIGN KEY (`u_id`) REFERENCES `users` (`u_id`),
+  ADD CONSTRAINT `box_ibfk_2` FOREIGN KEY (`u_id`) REFERENCES `user` (`u_id`),
   ADD CONSTRAINT `box_ibfk_1` FOREIGN KEY (`v_id`) REFERENCES `vocabulary` (`v_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
