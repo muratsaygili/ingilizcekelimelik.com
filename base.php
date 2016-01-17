1aa131f52c9c41cafc25eafc688dd9f3b21cddc1
Oguzhan Tasci <oguzhantasc@gmail.com>
2016-01-17 22:26:35 +0200

    closed #13

---
+++ base.php
@@ -0,0 +1,17 @@
+<?php 
+	
+	echo '<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="master.css">
+	<meta charset="utf-8">
+</head>
+<body>
+	<div class="nav">
+		<div>
+		<a href="index.php">ingilizce kelimelik</a>
+		<a class="right" href="logout.php">Çıkış</a>
+		</div>
+	</div>';
+
+ ?>
