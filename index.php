acd5453610be87b7b5545ed29601faac66702a32
Oguzhan Tasci <oguzhantasc@gmail.com>
2016-01-17 22:28:48 +0200

    fixed link path

--- index.php
+++ index.php
@@ -7,21 +7,13 @@
 <body>
 	<div class="nav">
 		<div>
-			<a href="index.html">ingilizce kelimelik</a>
-		<a class="right" href="kayit.php">Kayıt Ol</a>
-		<a class="right2" href="giris.php">Giriş Yap</a>
+		<a href="index.php">ingilizce kelimelik</a>
+		<a class="right" href="register.php">Kayıt Ol</a>
+		<a class="right2" href="login.php">Giriş Yap</a>
 		</div>
 	</div>
 	<div class="main">
-		<div class="sol">
-		<h3 class="center">Panel</h3>
-		<ul>
-			<li><a class="a-white" href="">Random Box</a></li>
-			<li><a class="a-white" href="">Create Box</a></li>
-			<li><a class="a-white" href="">Show Your Boxes</a></li>
-
-		</ul>
-	</div>
+		
 	<ul class="icerik">
         <div id="resim-don">
             <li class="resim">
@@ -33,6 +25,22 @@
             </li>
         </div>
     </ul>
+	
+	</div>
+	<div class="oneri">
+
+		<h3 class="center">Recommended Boxes</h3>
+	</div>
+	<div class="oneri-box center"><a class="a-white" href="">Box 1</a></div>
+	<div class="sol">
+		<h3 class="center">Panel</h3>
+		<ul>
+			<li><a class="a-white" href="">Random Box</a></li>
+			<li><a class="a-white" href="">Create Box</a></li>
+			<li><a class="a-white" href="">Show Your Boxes</a></li>
+
+		</ul>
+	</div>
 	<div class="gobek2">
 		<a href="">Random Vocabulary</a>
 		<br>
@@ -40,10 +48,5 @@
 		<br>
 		<a href="">Turkish - English Mode</a>
 	</div>
-	</div>
-	<div class="oneri">
-		<h3 class="center">Recommended Boxes</h3>
-	</div>
-	<div class="oneri-box center"><a class="a-white" href="">Box 1</a></div>
 </body>
 </html>
